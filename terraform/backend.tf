211664f861b45020158133da37f7a89a15baaa93
nanda8555 <nandakishore2432000@gmail.com>
2025-10-28 00:13:50 -0400

    removed terraform

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "vubber-web-terraform-artifacts-v2"
-    key            = "terraform.tfstate"
-    region         = "us-west-1"
-    encrypt        = true
-  }
-}
